3add7544e8f5d28e0797fb005da9d8c8d2cdf721
Dmrchk <105112928+Dmrchk@users.noreply.github.com>
2023-04-18 00:12:14 +0200

    Add files via upload

---
+++ note/src/main/java/com/od/dev/notes/NoteApp.java
@@ -0,0 +1,12 @@
+package com.od.dev.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class NoteApp {
+	public static void main(String[] args) {
+		SpringApplication.run(NoteApp.class, args);
+	}
+}
---
+++ note/src/test/java/com/od/dev/notes/NoteAppTests.java
@@ -0,0 +1,13 @@
+package com.od.dev.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
